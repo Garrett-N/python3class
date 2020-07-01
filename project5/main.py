52dcca6313e713ef559f004373ce1599e51ddd88
Garrett Neuenkirchen <garrettn@gmail.com>
2020-07-01 15:11:07 -0500

    Getting started on playing around with web.py

---
+++ project5/main.py
@@ -0,0 +1,14 @@
+import web
+
+urls = (
+    '/(.*)', 'index'
+)
+
+app = web.application(urls, globals())
+
+class index:
+    def GET(self, name):
+        return "<h1>Hello " + name + '. How are you today?</h1>'
+
+if __name__ == "__main__":
+    app.run()
