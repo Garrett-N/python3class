909fda6f327bd05bc675f3b32f69ec6afc4ede3a
Garrett Neuenkirchen <garrettn@gmail.com>
2020-07-24 00:25:49 -0500

    Added a login page / model and wired the controller.

--- CodeWizard/Controller.py
+++ CodeWizard/Controller.py
@@ -1,9 +1,12 @@
 import web
+from Models import RegisterModel, LoginModel
 
 urls = (
     '/', 'Home',
     '/register', 'Register',
-    '/postregistration', 'PostRegistration'
+    '/postregistration', 'PostRegistration',
+    '/login', 'Login',
+    '/check-login', 'CheckLogin'
 )
 
 render = web.template.render("Views/Templates", base="MainLayout")
@@ -19,10 +22,28 @@ class Register:
     def GET(self):
         return render.Register()
 
+class Login:
+    def GET(self):
+        return render.Login()
+
 class PostRegistration:
     def POST(self):
         post_data = web.input()
-        return post_data
+
+        reg_model = RegisterModel.RegisterModel()
+        reg_model.insert_user(post_data)
+        return post_data.username
+
+class CheckLogin:
+    def POST(self):
+        data = web.input()
+        login = LoginModel.LoginModel()
+        isCorrect = login.check_user(data)
+
+        if isCorrect:
+            return isCorrect
+
+        return "error"
 
 
 if __name__ == "__main__":
