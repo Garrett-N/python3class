01af0d285bbc661aad269fec28bf3fc9d62b97ba
Garrett Neuenkirchen <garrettn@gmail.com>
2020-08-13 15:19:32 -0500

    altering a QLabel

--- calculator/main.py
+++ calculator/main.py
@@ -11,31 +11,35 @@ class MainWindow(QWidget):
         self.counter = 0
 
     def init_ui(self):
-        label = QLabel("Name: ")
-        name_input = QLineEdit()
+        self.text_label = QLabel("There has been no name entered, so I can do anything yet.")
+        self.label = QLabel("Name: ")
+        self.name_input = QLineEdit()
         self.button = QPushButton("Clicked: 0 ")
-        self.button.clicked.connect(self.clickedButton)
 
+        self.button.setText("Set Name")
+        self.button.clicked.connect(self.alterName)
 
         h = QHBoxLayout()
         h.stretch(1)
-        h.addWidget(label)
-        h.addWidget(name_input)
+        h.addWidget(self.label)
+        h.addWidget(self.name_input)
 
         v = QVBoxLayout()
-        v.addStretch(1)
+        v.addWidget(self.text_label)
         v.addLayout(h)
         v.addWidget(self.button)
 
         self.setLayout(v)
 
-        self.setWindowTitle("Horizontal Layout")
+        self.setWindowTitle("Nothing has been clicked")
         self.show()
 
-    def clickedButton(self):
-        self.counter += 1
-        print("This button has been clicked.")
-        self.button.setText("Clicked: " + str( self.counter))
+    def alterName(self):
+        inputted_text = self.name_input.text()
+        our_string = "You've entered " + inputted_text
+        self.text_label.setText(our_string)
+        self.setWindowTitle(inputted_text)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
