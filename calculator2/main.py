9ace91fe46fa96fb89d388104f535d63a550484b
Garrett Neuenkirchen <garrettn@gmail.com>
2020-08-13 20:11:02 -0500

    Added buttons to grid and a result display

--- calculator2/main.py
+++ calculator2/main.py
@@ -12,15 +12,31 @@ class Application(QWidget):
 
     def CreateApp(self):
         grid = QGridLayout()
-        button1 = QPushButton("One")
-        button2 = QPushButton("Two")
-        button3 = QPushButton("Three")
-        button4 = QPushButton("My last button")
+        results = QLineEdit()
 
-        grid.addWidget(button1, 0, 0, 1, 1)
-        grid.addWidget(button2, 0, 1, 1, 1)
-        grid.addWidget(button3, 0, 2, 1, 1)
-        grid.addWidget(button4, 1, 0, 1, 2)
+        buttons = ["AC", "C", "CE", "/",
+                   7, 8, 9, "*",
+                   4, 5, 6, "-",
+                   1, 2, 3, "+",
+                   0, ".", "="]
+
+        row = 1
+        col = 0
+
+        grid.addWidget(results, 0, 0, 1, 4)
+
+        for button in buttons:
+            if col > 3:
+                col = 0
+                row += 1
+            if button == 0:
+                grid.addWidget(QPushButton(str(button)), row, col, 1, 2)
+                col += 1
+            else:
+                grid.addWidget(QPushButton(str(button)), row, col, 1, 1 )
+            col += 1
+
+
 
         self.setLayout(grid)
         self.show()
