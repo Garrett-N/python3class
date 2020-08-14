0fd99f78d09b9e6c5e6091c844eb7b8ec99533b8
Garrett Neuenkirchen <garrettn@gmail.com>
2020-08-13 19:57:10 -0500

    Creating a grid and adding some buttons

--- calculator2/main.py
+++ calculator2/main.py
@@ -8,6 +8,21 @@ class Application(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Calculator")
+        self.CreateApp()
+
+    def CreateApp(self):
+        grid = QGridLayout()
+        button1 = QPushButton("One")
+        button2 = QPushButton("Two")
+        button3 = QPushButton("Three")
+        button4 = QPushButton("My last button")
+
+        grid.addWidget(button1, 0, 0, 1, 1)
+        grid.addWidget(button2, 0, 1, 1, 1)
+        grid.addWidget(button3, 0, 2, 1, 1)
+        grid.addWidget(button4, 1, 0, 1, 2)
+
+        self.setLayout(grid)
         self.show()
 
 if __name__ == '__main__':
