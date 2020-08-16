bd0a6250bfa37ca59c01668e449e803271de9ba3
Garrett Neuenkirchen <garrettn@gmail.com>
2020-08-16 11:46:10 -0500

    Adding addressbar to the layout and overriding a mouse handler plus some layout improvements.

--- web-browser/main.py
+++ web-browser/main.py
@@ -8,23 +8,50 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QP
 from PyQt5.QtGui import (QIcon, QWindow, QImage)
 from PyQt5.QtCore import *
 
+class AddressBar(QLineEdit):
+    def __init__(self):
+        super().__init__()
+
+    def mousePressEvent(self, e):
+        self.selectAll()
+
+
 class App(QFrame):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Web Browser")
-        self.setBaseSize(1366, 768)
         self.CreateApp()
+        self.setBaseSize(1366, 768)
 
     def CreateApp(self):
         self.layout = QVBoxLayout()
-        self.tabbar = QTabBar()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0,0,0,0)
+
+        # Create Tabs
+        self.tabbar = QTabBar(movable=True, tabsClosable=True)
+        self.tabbar.tabCloseRequested.connect(self.CloseTab)
         self.tabbar.addTab("Tab 1")
         self.tabbar.addTab("Tab 2")
         self.tabbar.setCurrentIndex(0)
 
+        # Create Addressbar
+        self.Toolbar = QWidget()
+        self.ToolbarLayout = QHBoxLayout()
+        self.addressbar = AddressBar()
+
+        self.Toolbar.setLayout(self.ToolbarLayout)
+        self.ToolbarLayout.addWidget(self.addressbar)
+
         self.layout.addWidget(self.tabbar)
+        self.layout.addWidget(self.Toolbar)
+
         self.setLayout(self.layout)
         self.show()
+
+    def CloseTab(self, i):
+        self.tabbar.removeTab(i)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
